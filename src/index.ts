3ce9807ccac54afdf962b06c0cb1fe46a56f9330
miniaggarwal <priyankaagarwal608@gmail.com>
2025-08-31 00:28:36 +0530

    added the context to the previous chat

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,47 @@ import * as dotenv from "dotenv";
 
 dotenv.config();
 
-console.log(process.env.OPENAI_KEY);
+// console.log(process.env.OPENAI_KEY);
 
-const openAI = new OpenAI({apiKey : process.env.OPENAI_KEY
-});
+const openAI = new OpenAI({ apiKey: process.env.OPENAI_KEY });
 
+const context: OpenAI.Chat.Completions.ChatCompletionMessageParam[] = [
+    {
+        role: "system",
+        content: `You are a good and humble assistant. You always start the conversation with "Hi!!" and end the conversation with "Please let me know if I can assit you further" `
+    },
+];
+
+const createChat = async () => {
+    const response = await openAI.chat.completions.create({
+        model: "chatgpt-4o-latest",
+        messages: context
+    });
+
+    const responseMessage = response.choices[0]?.message;
+    if (responseMessage) {
+        context.push(responseMessage);
+    }
+
+
+    console.log(response.choices[0]?.message.content);
+
+};
+
+//to read user input 
+process.stdin.addListener("data", async (input) => {
+    const userInput = input.toString().trim();
+    context.push({
+        role: "user",
+        content: userInput
+
+    })
+
+    await createChat();
+})
+
+//to create a chat without context
+/*
 process.stdin.addListener("data", async (input: Buffer) => {
     const userInput: string = input.toString().trim();
     const response = await openAI.chat.completions.create(
@@ -36,3 +72,4 @@ process.stdin.addListener("data", async (input: Buffer) => {
   
 });
 
+*/
